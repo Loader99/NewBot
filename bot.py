5f60346253bf51e3c3e21df54d780198be5d880c
Loader99 <basicnollageee@gmail.com>
2026-02-13 23:22:54 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import subprocess
 import shlex
 import os
 
-API_TOKEN = 'YOUR_BOT_TOKEN' # यहाँ अपना टोकन डालें
 ADMIN_ID = 7899583720        # अपना Telegram ID यहाँ डालें
 bot = telebot.TeleBot("8278930868:AAEFn-tRoVH-aP5x8OnxONoUKiq3k6r9rEw")
 process = None
